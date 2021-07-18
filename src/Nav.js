4e40f36ea58c791cdae9f69159ab5d3121316412
Patel <brij.patel@thomsonreuters.com>
2021-07-18 15:22:30 -0400

    Go

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+import './Nav.css'
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY > 100){
+                console.log('THERE')
+                handleShow(true)
+            }
+            else{
+                handleShow(false);
+            }
+        });
+
+        return () => {
+            window.removeEventListener("scroll");
+         }
+    },[])
+
+    return (
+        <div className = {`nav ${show && "nav_black"} `}>
+            <img
+            className = "nav_logo"
+            src = "https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+            alt = "Netflix Logo"
+            ></img>
+
+        <img
+         className = "nav_avatar"
+         src = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+         alt = "Netflix Logo"
+        ></img>
+        </div>
+    )
+}
+
+export default Nav
